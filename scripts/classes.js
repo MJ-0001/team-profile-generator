408d11e24339b1ae08f6ad5fbe868a2cb90dcfb3
Matthew Jones <mejones92@gmail.com>
2021-03-01 23:40:36 +0000

    Fixed issues with constructor not returning property.

--- scripts/classes.js
+++ scripts/classes.js
@@ -4,41 +4,27 @@ class Employee {
   this.id = id;
   this.email = email;
   }
-}
+};
 
 class Manager extends Employee {
-  constructor(name, id, email, officeNum) {
+  constructor(name, id, email, phone) {
     super(name, id, email);
-    this.officeNum = officeNum;
+    this.phone = phone;
   }
-}
+};
 
-Manager.prototype.role = function() {
-  return 'Manager';
-}
-Manager.prototype.role = function() {
-  return 'Manager';
-}
 class Engineer extends Employee {
   constructor(name, id, email, git) {
     super(name, id, email);
     this.git = git;
   }
-}
-
-// Engineer.prototype.role = function() {
-//   return 'Engineer';
-// }
+};
 
 class Intern extends Employee {
   constructor(name, id, email, school) {
     super(name, id, email);
     this.school = school;
   }
-}
+};
 
-// Intern.prototype.role = function() {
-//   return 'Intern';
-// }
-
-module.exports = Manager, Engineer, Intern;
+module.exports = { Manager, Engineer, Intern };
