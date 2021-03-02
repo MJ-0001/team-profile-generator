dcab564fbe441dd1dccd8080f46129585ae1d7de
Matthew Jones <mejones92@gmail.com>
2021-03-02 20:13:55 +0000

    'it' descriptions made clearer.

--- __tests__/employee.test.js
+++ __tests__/employee.test.js
@@ -6,19 +6,19 @@ describe('Employee', () => {
       expect(typeof(employeeTest)).toBe('object');
   });
 
-  it('should construct the name property as key value pair', () => {
+  it('should construct the name property when called', () => {
     const nameTest = 'Matt';
     const employeeTest = new e.Employee(nameTest);
     expect(employeeTest.name).toBe(nameTest);
   });
 
-  it('should construct the id property as key value pair', () => {
+  it('should construct the id property when called', () => {
     const idTest = 1234;
     const employeeTest = new e.Employee('test', idTest);
     expect(employeeTest.id).toBe(idTest);
   });
 
-  it('should construct the email property as key value pair', () => {
+  it('should construct the email property when called', () => {
     const emailTest = 'test@gmail.com';
     const employeeTest = new e.Employee('test', 'test', emailTest);
     expect(employeeTest.email).toBe(emailTest);
